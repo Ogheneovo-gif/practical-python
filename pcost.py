8be3787a721fce9e1fc88924252c2952e3309794
Ogheneovo Mclarry Eduiyovwiri <meduiyovwiri@mun.ca>
2020-07-24 03:07:19 -0230

    Exercise 1.27

--- pcost.py
+++ pcost.py
@@ -1,21 +1,14 @@
-# pcost.py
+## pcost.py
 
-import report1
+total_cost = 0.0
 
-def portfolio_cost(filename):
-    '''
-    Computes the total cost (shares*price) of a portfolio file
-    '''
-    portfolio = report.read_portfolio(filename)
-    return sum([s['shares'] * s['price'] for s in portfolio])
+with open('../../Work/Data/portfolio.csv', 'rt') as f:
+    headers = next(f)
+    for line in f:
+        row = line.split(',')
+        nshares = int(row[1])
+        price = float(row[2])
+        total_cost += nshares * price
 
-def main(args):
-    if len(args) != 2:
-        raise SystemExit('Usage: %s portfoliofile' % args[0])
-    filename = args[1]
-    print('Total cost:', portfolio_cost(filename))
-
-if __name__ == '__main__':
-    import sys
-    main(sys.argv)
+print('Total cost', total_cost)
 
